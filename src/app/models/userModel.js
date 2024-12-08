f59664926b7cf9e51f6d4c0151e22195094b0174
Sushanth Nandeti <sushanthnandeti96@gmail.com>
2024-12-07 22:47:38 -0500

    DB Model Update 1.0

--- src/app/models/userModel.js
+++ src/app/models/userModel.js
@@ -1,3 +1,4 @@
+import { verify } from "crypto";
 import mongoose from "mongoose";
 import { unique } from "next/dist/build/utils";
 import { type } from "os";
@@ -25,7 +26,11 @@ const userSchema = new mongoose.Schema({
     isAdmin: {
         type: Boolean,
         default: false
-    }
+    },
+    forgotPasswordToken: String, 
+    forgotPasswordTokenExpiry: Date, 
+    verifyToken : String, 
+    verifyTokenExpiry: Date,
 })
 
 const User = mongoose.models.users || mongoose.model("User", userSchema);
